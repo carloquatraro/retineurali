70390369cfa301efb381273bd46b846512e43d9e
carloquatraro <carlo.quatraro1@gmail.com>
2024-02-01 17:17:38 +0100

    Work almost done
    This reverts commit 0a32ac5f

--- funzioni.py
+++ funzioni.py
@@ -1,3 +1,4 @@
+# Importazione delle librerie da utilizzare
 import numpy as np
 from tqdm import tqdm
 from skimage.transform import resize
@@ -7,11 +8,15 @@ from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, BatchNormali
 from keras.regularizers import l1, l2
 from sklearn.utils import shuffle
 from keras.optimizers import Adam
-from keras.losses import binary_crossentropy, sparse_categorical_crossentropy
+from keras.losses import binary_crossentropy, sparse_categorical_crossentropy, categorical_crossentropy
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score,precision_score,recall_score
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import pandas as pd
 
-
+# Funzione di ridimensionamento delle immagini
 def resize_images(X_data, target_size:tuple,iteratore:list, N:int):
     X_dataresized = np.empty((N, target_size[0], target_size[1], 1))
     for img_idx in tqdm(iteratore):
@@ -20,16 +25,18 @@ def resize_images(X_data, target_size:tuple,iteratore:list, N:int):
         X_dataresized[img_idx,:,:,:] = resized_img[None,..., None]
     return X_dataresized
 
+# Funzione di definizione del dice coefficient
 def dice_coef(y_true, y_pred, smooth=0):
   y_true_f = tf.reshape(tf.cast(y_true, tf.float32),[-1])
   y_pred_f = tf.reshape(tf.cast(y_pred, tf.float32), [-1])
   return (2 * tf.reduce_sum(y_true_f * y_pred_f) + smooth) / (
             tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
 
+# Funzione di definizione del dice loss
 def dice_loss(y_true, y_pred):
   return 1 - dice_coef(y_true, y_pred, smooth=2000)
 
-
+# Funzione di definizione del modello per la segmentazione
 def easy_cnn(input_shape):
     model = Sequential()
 
@@ -57,6 +64,9 @@ def easy_cnn(input_shape):
     model.add(Conv2D(1, kernel_size=(1, 1), activation='sigmoid', padding='same'))
 
     return model
+
+# Funzione di definizione del modello per la classificazione
+
 def classification_cnn(input_shape):
     model_c = Sequential()
     model_c.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
@@ -66,15 +76,18 @@ def classification_cnn(input_shape):
     model_c.add(Flatten())
     model_c.add(Dense(256, activation='relu'))
     model_c.add(Dense(128, activation='relu'))
-    model_c.add(Dense(3, activation='softmax'))
+    model_c.add(Dense(3, activation='sigmoid'))
     return model_c
 
+
+# Funzione di cross validation per la segementazione
 def cross_valid(model_fun,N_folds,data,masks,h):
 
     kf = KFold(n_splits=N_folds, shuffle=True)
 
     results = []
     for f, (train_index, test_index) in enumerate(kf.split(data)):
+        print('f=', f + 1)
         dice_c = np.empty(len(test_index))
 
         testData = data[test_index, :, :, :]
@@ -85,24 +98,44 @@ def cross_valid(model_fun,N_folds,data,masks,h):
         model = model_fun(data.shape[1:])
         model.compile(optimizer=Adam(0.001), loss=binary_crossentropy, metrics=['accuracy'])
 
-        model.fit(trainData, trainMasks, batch_size=h["batch_size"], epochs=h["epochs"], validation_split=h["validation_split"],verbose=1)
+        history = model.fit(trainData, trainMasks, batch_size=h["batch_size"], epochs=h["epochs"], validation_split=h["validation_split"],verbose=1)
 
         for n in range(len(test_index)):
             est_mask = np.squeeze(model.predict(testData[n, :, :, :][None, ...]) > 0.7)
             dice_c[n] = dice_coef(tf.convert_to_tensor(testMasks[n, :, :, 0].astype(np.float32)),tf.convert_to_tensor(est_mask.astype(np.float32)))
 
+        plt.figure(figsize=(8, 8))
+        plt.subplot(121)
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.ylim([0, 2])
+        plt.gca().set_aspect(50)
+        plt.subplot(122)
+        plt.imshow(testData[n, :, :, :], cmap='gray')
+        plt.imshow(est_mask, cmap='gray', alpha=0.4)
+        # plt.axis('off')
+        plt.show()
+
         results.append(np.mean(dice_c))
         print(np.mean(dice_c))
-        print ('f=',f+1)
-        del model, trainData, trainMasks, testData, testMasks, est_mask
+        del model, trainData, trainMasks, testData, testMasks
+
     return results
 
 
+# Funzione di nested cross validation per la classificazione
 def Ncross_valid(model_fun, data, masks):
 
-    kf_out = KFold(n_splits=2, shuffle=True)
-    kf_in = KFold(n_splits=2, shuffle=True)
-    risultato = []
+    kf_out = KFold(n_splits=3, shuffle=True)
+    kf_in = KFold(n_splits=3, shuffle=True)
+    # risultato = []
+    accuracy = []
+    precision = []
+    recall = []
     for f, (dev_index, test_index) in enumerate(kf_out.split(data)):
         testData = data[test_index, :, :, :]
         testEtic = masks[test_index]
@@ -122,8 +155,11 @@ def Ncross_valid(model_fun, data, masks):
                 model.compile(optimizer=Adam(g), loss=sparse_categorical_crossentropy, metrics=['accuracy'])
                 b_size = {'B_size': [64, 128]}
                 for k in b_size['B_size']:
+                    print ('Batch=',k)
+                    print('g=', g)
+                    print('i=', i )
 
-                    model.fit(tData, tEtic, batch_size=k, epochs=1, validation_split=0.1,verbose=1)
+                    model.fit(tData, tEtic, batch_size=k, epochs=5, validation_split=0.1,verbose=1)
                     score = model.evaluate(valData, valEtic, verbose=0)
 
                     if score[1]> acc_best:
@@ -133,11 +169,18 @@ def Ncross_valid(model_fun, data, masks):
 
         model = model_fun(devData.shape[1:])
         model.compile(optimizer=Adam(best_params['learning_rate']), loss=sparse_categorical_crossentropy, metrics=['accuracy'])
-        model.fit(devData, devEtic, batch_size=best_params['batch_size'], epochs=1, validation_split=0.1,verbose=1)
+        model.fit(devData, devEtic, batch_size=best_params['batch_size'], epochs=5, validation_split=0.1,verbose=1)
 
         score = model.evaluate(testData, testEtic, verbose=0)
-        risultato[0] = risultato.append (accuracy_score(testEtic, model.predict(testData)))
-        risultato[1] = risultato.append (precision_score(testEtic, model.predict(testData)))
-        risultato[2] = risultato.append (recall_score(testEtic, model.predict(testData)))
 
-    return risultato
+        # print (testEtic.shape)
+        # print ( model.predict(testData).shape)
+
+        predizioneEtic = np.argmax(model.predict(testData),axis=-1)
+
+
+        accuracy.append (accuracy_score(testEtic, predizioneEtic))
+        precision.append (precision_score(testEtic, predizioneEtic, average = 'weighted'))
+        recall.append (recall_score(testEtic, predizioneEtic, average = 'weighted'))
+
+    return accuracy, precision, recall
--- main.py
+++ main.py
@@ -1,30 +1,31 @@
+# Importazione delle librerie da utilizzare
 import numpy as np
 import random as rd
 from skimage import io as skio
 from sklearn.utils import shuffle
 from funzioni import *
-
-# Libraries for Deep Learning models
 import tensorflow as tf
-from keras import Sequential       # for sequential API
-from keras.models import Model     # for functional API
+from keras import Sequential
+from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, Activation, Add, Concatenate
 from keras.regularizers import l1, l2
 from keras.optimizers import Adam
 from keras.losses import binary_crossentropy
 from skimage.transform import resize
 from keras.losses import SparseCategoricalCrossentropy
-
 from sklearn.model_selection import KFold
-
-# Other utilities
 from tqdm import tqdm
 from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import pandas as pd
 
+# Directories del dataset
 maindirC="./Lung_dataset/COVID/"
 maindirN="./Lung_dataset/Normal/"
 maindirVP="./Lung_dataset/Viral_Pneumonia/"
 
+# Creazione degli array per le immagini e le maschere
 N = 1000
 sbj_ids = [*range(1, N+1)]
 X_data = np.empty((3*N,299,299,1))
@@ -32,37 +33,45 @@ y_data = np.empty((3*N,256,256,1))
 input_shapeX = np.array ((299,299,1))
 input_shapey = np.array ((256,256,1))
 
+# Caricamento delle immagini e delle maschere
 for id in tqdm(sbj_ids):
-  #Leggo le immagini covid
+
+  # Leggo le immagini covid
   imgC = skio.imread(maindirC+f"/images/COVID-{id}.png").astype('float')
   maskC = skio.imread(maindirC+f"/masks/COVID-{id}.png", as_gray=True).astype('bool')
   maskC = (maskC).astype('float')
-  #leggo le immagini normali
+
+  # Leggo le immagini normali
   imgN = skio.imread(maindirN + f"/images/Normal-{id}.png").astype('float')
   maskN = skio.imread(maindirN + f"/masks/Normal-{id}.png", as_gray=True).astype('bool')
   maskN = (maskN).astype('float')
-  #leggo le immagini vireal pneumonia
+
+  # Leggo le immagini viral pneumonia
   imgVP = skio.imread(maindirVP + f"/images/Viral Pneumonia-{id}.png", as_gray=True).astype('float')
   maskVP = skio.imread(maindirVP + f"/masks/Viral Pneumonia-{id}.png", as_gray=True).astype('bool')
   maskVP = (maskVP).astype('float')
+
   #Assegno le immagini lette al vettore X_data
   #Prima le imamgini covid
   X_data[id-1,:,:,:] = imgC[None,...,None]
   y_data[id-1,:,:,:] = maskC[None,...,None]/maskC.max()
+
   #Poi le immagini normali
   X_data[id - 1 + 1000, :, :, :] = imgN[None, ..., None]
   y_data[id - 1 + 1000, :, :, :] = maskN[None, ..., None] / maskN.max()
+
   #Infine le immagini viral pneumonia
   X_data[id - 1 + 2000, :, :, :] = imgVP[None, ..., None]
   y_data[id - 1 + 2000, :, :, :] = maskVP[None, ..., None] / maskVP.max()
 
   del imgC, maskC, imgN, maskN, imgVP, maskVP
 
+# Verifico le dimensioni degli array
 print(X_data.shape)
 print(y_data.shape)
 
 '''
-# Provo a printare delle immagini casuali 
+# Print casuale di una immagine 
 numero=rd.randrange(3*N)
 print(numero)
 plt.imshow(X_data[numero,:,:,0],cmap='gray')
@@ -71,45 +80,43 @@ plt.show()
 '''
 
 # Ridimensionamento delle immagini e maschere
-iteratore = [*range(0, 3*N)]
+M = 3000
+iteratore = [*range(0, M)]
 target_size = (128,128)
-X_dataresized = np.empty((3*N,128,128,1))
-y_dataresized = np.empty((3*N,128,128,1))
+X_dataresized = np.empty((M,128,128,1))
+y_dataresized = np.empty((M,128,128,1))
 
-# Creazione X_dataresized
-X_dataresized=resize_images(X_data, target_size,iteratore,3*N)
+# Creazione e riempimento di X_dataresized con le immagini ridimensionate
+X_dataresized=resize_images(X_data, target_size,iteratore,M)
 print(X_dataresized.shape)
 
 '''
-# Provo a printare delle immagini casuali
-numero=rd.randrange(3*N)
+# Print casuale di una immagine ridimensionata 
+numero=rd.randrange(M)
 print(numero)
 plt.imshow(X_dataresized[numero,:,:,0],cmap='gray')
 plt.axis('off')
 plt.show()
 '''
 
-# Creazione y_dataresized
-y_dataresized=resize_images(y_data, target_size,iteratore,3*N)
+# Creazione e riempimento di y_dataresized con le maschere ridimensionate
+y_dataresized=resize_images(y_data, target_size,iteratore,M)
 print(y_dataresized.shape)
 
 '''
-# Provo a printare delle immagini casuali
-numero=rd.randrange(3*N)
+# Print casuale di una maschera ridimensionata
+numero=rd.randrange(M)
 print(numero)
 plt.imshow(y_dataresized[numero,:,:,0],cmap='gray')
 plt.axis('off')
 plt.show()
 '''
 
-'''
-# Creazione del modello
+# Creazione del modello per segmentazione e print del suo summary
 model = easy_cnn(X_dataresized.shape[1:])
-
-#Print the summary of the model architecture
 model.summary()
 
-
+# Definizione degli iperparametri utilizzati per la segmentazione
 N_folds = 5
 hyperparams = {
     "batch_size":10,
@@ -117,27 +124,32 @@ hyperparams = {
     "validation_split":0.1,
 }
 
+# Segmentazione
 results = cross_valid(easy_cnn,N_folds,X_dataresized,y_dataresized,hyperparams)
-'''
 
+# Creazione del modello per classificazione e print del suo summary
 model_c = classification_cnn(X_dataresized.shape[1:])
 model_c.summary()
 
+# Definizione del numero di classi e creazione del vettore contente le etichette
 nclassi = 3
 COVID=np.zeros(N)
 Normal=np.ones(N)
 ViralPneumonia=2*np.ones(N)
 etichette = np.concatenate((COVID,Normal,ViralPneumonia),axis=0)
 
-#Ncross valid -> risultato = (accuracy:lista, precision:lista, recall:lista)
+# Classificazione con estrapolazione delle metriche
+accuracy,precision,recall = Ncross_valid(classification_cnn, X_dataresized, etichette)
 
-risultato = Ncross_valid(classification_cnn, X_dataresized, etichette)
-average_accuracy = np.mean(risultato[0])
-average_precision = np.mean(risultato[1])
-average_recall = np.mean(risultato[2])
+# Media delle metriche richieste e print
+average_accuracy = np.mean(accuracy)
+average_precision = np.mean(precision)
+average_recall = np.mean(recall)
 
 print(f'Accuracy: {average_accuracy}')
 print(f'Precision: {average_precision}')
 print(f'Recall: {average_recall}')
+
+
 
 
